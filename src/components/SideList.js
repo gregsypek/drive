e9fab5556e6aac447aeda3e9a5108bfdd9f266b8
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-03-10 09:53:27 +0100

    add filterCurrentDisplayItems method

--- src/components/SideList.js
+++ src/components/SideList.js
@@ -1,14 +1,44 @@
-import React from "react";
-import folder from "../pics/folder.png";
-import dots from "../pics/three-dots.png";
+/* eslint-disable no-unused-expressions */
+import React, { useEffect } from "react";
 import "../css/SideList.css";
 // import {toast} from 'react-hot-toast';
 
 import { useStateContext } from "../context/StateContext";
 
 export default function SideList() {
-	const { items, setItems } = useStateContext();
-	console.log("🚀 ~ file: SideList.js:11 ~ SideList ~ items:", items)
+	const {
+		items,
+		filterCurrentDisplayItems,
+		filteredItems,
+		currentFolderId,
+		setCurrentFolderId,
+	} = useStateContext();
+	console.log("🚀 ~ file: SideList.js:11 ~ SideList ~ items:", items);
+
+
+
+	const sortAndFilter = (arr) => {
+		const sorted = arr.sort((a, b) => {
+			if (a.itemText < b.itemText) return -1;
+			if (a.itemText > b.itemText) return 1;
+			return 0;
+		});
+		const folders = sorted.filter((item) => item.type === "folder");
+		const projects = sorted.filter((item) => item.type === "project");
+		return [...folders, ...projects];
+	};
+	const handleClick = (type, id) => {
+		console.log("🚀 ~ file: SideList.js:41 ~ handleClick ~ id222:", id)
+		console.log("🚀 ~ file: SideList.js:41 ~ handleClick ~ type222:", type)
+		if (type === "folder") {
+			setCurrentFolderId(id);
+			filterCurrentDisplayItems(currentFolderId);
+		}
+	};
+
+	useEffect(() => {
+		filterCurrentDisplayItems(currentFolderId);
+	}, [currentFolderId]);
 
 	return (
 		<>
@@ -16,11 +46,20 @@ export default function SideList() {
 				<button id="linkBtn">
 					<p>New</p>
 				</button>
-				<div id="sideListOpt">	
-						<ul>						
-							{items.map((item) => (<li className={item.type === 'project' ? 'folder' : 'file'} key={item.id}>{item.itemText}</li>))}		
-						</ul>	
+				<div id="sideListOpt">
+			
+					<ul>
+						{sortAndFilter(filteredItems).map((item) => (
+							<li
+								onClick={() => handleClick(item.type, item.id)}
+								className={item.type === "folder" ? "folder" : "project"}
+								key={item.id}
+							>
+								{item.itemText}
+							</li>
+						))}
 					
+					</ul>
 				</div>
 			</div>
 		</>
--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,35 +1,61 @@
-import React, {createContext, useContext, useState} from 'react'
-import dataDirs from "../data/dirs.json"
-import dataProjects from "../data/projects.json"
+import React, { createContext, useContext, useState } from "react";
+import dataDirs from "../data/dirs.json";
+import dataProjects from "../data/projects.json";
 
 const Context = createContext();
 
-export const StateContext =({children}) => {  
-  
-  const dirs = dataDirs.dirs.map(item=> ({
-    id: item.id,
-    type: "folder",
-    itemText: item.name,
-    currentFolderId:item.parentDirId
-  }))
-  const projects = dataProjects.projects.map(project =>({
-    id:project.id,
-    type: "project",
-    itemText: project.name,
-   
-  }))
-  
-const [items, setItems] = useState([...dirs,...projects])
-return (
-  <Context.Provider value={{
-    items, 
-    setItems,  
-  }}
-  >
-    {children}
-  </Context.Provider>
-)
+export const StateContext = ({ children }) => {
+	const dirs = dataDirs.dirs.map((item) => ({
+		id: item.id,
+		type: "folder",
+		itemText: item.name,
+    folderId:item.parentDirId
+	}));
+	const projects = dataProjects.projects.map((project) => ({
+		id: project.id,
+		type: "project",
+		itemText: project.name,
+    folderId:project.folderId
+	}));
 
-}
+	const [items, setItems] = useState([...dirs, ...projects]);
+	const [filteredItems, setFilteredItems] = useState([]);
+	const [currentFolderId, setCurrentFolderId] = useState(
+null
+	);
+  // "6affab85-552e-49ec-90cd-78bc65d2ffa1"
 
-export const useStateContext = ()=> useContext(Context)
+	const filterCurrentDisplayItems = (id) => {
+
+		const filterDirs = dirs.filter((item) => item.folderId === id);
+		console.log("🚀 ~ file: StateContext.js:31 ~ filterCurrentDisplayItems ~ filterDirs:", filterDirs)
+
+		const filterProjects = projects.filter(
+			(item) => item.folderId === id
+		);
+		console.log("🚀 ~ file: StateContext.js:36 ~ filterCurrentDisplayItems ~ filterProjects:", filterProjects)
+
+		console.log('filteredItems',[...filterDirs, ...filterProjects])
+		setFilteredItems([...filterDirs, ...filterProjects]);
+	};
+
+
+
+	return (
+		<Context.Provider
+			value={{
+				items,
+				setItems,
+        filterCurrentDisplayItems,
+        filteredItems,
+        setFilteredItems,
+        currentFolderId,
+        setCurrentFolderId
+			}}
+		>
+			{children}
+		</Context.Provider>
+	);
+};
+
+export const useStateContext = () => useContext(Context);
